0777b782e28afa8f5d16b65c47eec65aa5dac6f6
ghemmes <gchemmes20@gmail.com>
2023-03-28 21:00:30 -0700

    testing out app signup

---
+++ dist/js/mainauth.js
@@ -0,0 +1,90 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCBN-wifWLgZlNGnHRue2IWpFgUBosIs9A",
+    authDomain: "lawnadm-439f1.firebaseapp.com",
+    projectId: "lawnadm-439f1",
+    storageBucket: "lawnadm-439f1.appspot.com",
+    messagingSenderId: "820429638224",
+    appId: "1:820429638224:web:6873b35746ec5317a0df55",
+    measurementId: "G-VDX2NNMTN6"
+    };
+    firebase.initializeApp(config);
+    // firebase.analytics();
+
+
+    const auth = firebase.auth()
+
+
+
+
+
+  let signUpButton = document.getElementById('signup')
+  signUpButton.addEventListener("click", (e) => {
+    //Prevent Default Form Submission Behavior
+    e.preventDefault()
+    console.log("clicked")
+
+    var email = document.getElementById("inputEmail")
+    var password = document.getElementById("inputPassword")
+    
+    auth.createUserWithEmailAndPassword(email.value, password.value)
+    .then((userCredential) => {
+      location.reload();
+      // Signed in 
+      var user = userCredential.user;
+      console.log("user",user.email)
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("error code", errorCode)
+      console.log("error Message", errorMessage)
+    });
+  })
+
+
+
+
+  let signInButton = document.getElementById('signin')
+  signInButton.addEventListener("click", (e) => {
+    //Prevent Default Form Submission Behavior
+    e.preventDefault()
+    console.log("clicked")
+
+    var email = document.getElementById("inputEmail")
+    var password = document.getElementById("inputPassword")
+
+    auth.signInWithEmailAndPassword(email.value, password.value) 
+    .then((userCredential) => {
+      // location.reload();
+      // Signed in 
+      var user = userCredential.user;
+      console.log("user",user.email)
+      window.location = "test1.html";
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // alert("error code", errorCode)
+      alert( errorMessage)
+    });
+   })
+
+
+auth.onAuthStateChanged(function(user) {
+  if (user) {
+
+    var email = user.email
+  
+    var users = document.getElementById("user")
+    var text = document.createTextNode(email);
+
+    users.appendChild(text);
+
+    console.log(users)
+    //is signed in
+  } else {
+    //no user signed in
+  }
+})
